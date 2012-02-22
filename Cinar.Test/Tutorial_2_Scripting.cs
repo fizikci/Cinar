f845e5f2e47b18967a51470723f2fb2ac4d3ffcb
fizikci <fizikci@4843afb2-09fb-4a5a-bb7b-a1a0dfaeec16>
2012-02-22 19:48:24 +0000

    Application nonsense removed from CMS

---
+++ Cinar.Test/Tutorial_2_Scripting.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cinar.Scripting;
+using System.IO;
+
+namespace Cinar.Test
+{
+    public class Tutorial_2_Scripting
+    {
+        public static void Run()
+        {
+            Interpreter engine = new Interpreter(File.ReadAllText("d:\\deneme.csc"), null);
+            engine.SetAttribute("musteri", new Musteri { Ad="Microsoft"});
+            engine.Parse();
+            engine.Execute();
+
+            Console.WriteLine(engine.Output);
+        }
+    }
+
+    public class Musteri
+    {
+        public string Ad { get; set; }
+    }
+}
